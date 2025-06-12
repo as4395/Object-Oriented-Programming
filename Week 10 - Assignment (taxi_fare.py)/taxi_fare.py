5b33a7ebdad5bd0d3e0e0bbea16f7212651632b9
Abhiram S <abhi6945@gmail.com>
2025-06-11 22:58:29 -0400

    Update taxi_fare.py

--- Week 10 - Assignment (taxi_fare.py)/taxi_fare.py
+++ Week 10 - Assignment (taxi_fare.py)/taxi_fare.py
@@ -3,7 +3,11 @@ The script calculates the taxi fare based on the distance traveled in kilometers
 The base fare is $4.00 and $0.25 is added for every 140 meters traveled. 
 
 """
+
 def fare_calculator(distance_km):
+    if distance_km < 0:
+        return None  # Return None when a negative number is the input
+    
     base_fare = 4.00
     meters_traveled = distance_km * 1000 
     cost_per_140m = 0.25
@@ -15,6 +19,11 @@ def fare_calculator(distance_km):
 
 def user_input():
     distance = float(input("Enter the distance traveled in kilometers: "))
+    
+    if distance < 0:
+        print("Distance cannot be negative.")
+        return
+    
     fare = fare_calculator(distance)
     print(f"The total fare for traveling {distance} kilometers is ${fare:.2f}")
 
